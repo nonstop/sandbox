f79ac34ecf69647e25bab12ad29e1028d924aa44
Evgeny Ratnikov <ratnikov.ev@gmail.com>
2009-05-25 16:04:18 +0400

    more simple

--- java/jtree/FilteringJTree.java
+++ java/jtree/FilteringJTree.java
@@ -19,15 +19,6 @@ import java.awt.GridLayout;
 public class FilteringJTree extends JPanel {
     private JEditorPane htmlPane;
     private JTree tree;
-    private URL helpURL;
-
-    //Optionally play with line styles.  Possible values are
-    //"Angled" (the default), "Horizontal", and "None".
-    private static boolean playWithLineStyle = false;
-    private static String lineStyle = "Horizontal";
-    
-    //Optionally set the look and feel.
-    private static boolean useSystemLookAndFeel = false;
 
     public FilteringJTree() {
         super(new GridLayout(1,0));
@@ -41,11 +32,6 @@ public class FilteringJTree extends JPanel {
         tree = new JTree(top);
         tree.getSelectionModel().setSelectionMode
                 (TreeSelectionModel.SINGLE_TREE_SELECTION);
-
-        if (playWithLineStyle) {
-            System.out.println("line style = " + lineStyle);
-            tree.putClientProperty("JTree.lineStyle", lineStyle);
-        }
 
         //Create the scroll pane and add the tree to it. 
         JScrollPane treeView = new JScrollPane(tree);
@@ -103,15 +89,6 @@ public class FilteringJTree extends JPanel {
      * event dispatch thread.
      */
     private static void createAndShowGUI() {
-        if (useSystemLookAndFeel) {
-            try {
-                UIManager.setLookAndFeel(
-                    UIManager.getSystemLookAndFeelClassName());
-            } catch (Exception e) {
-                System.err.println("Couldn't use system look and feel.");
-            }
-        }
-
         //Create and set up the window.
         JFrame frame = new JFrame("FilteringJTree");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
