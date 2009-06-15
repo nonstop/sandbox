bf2dc36835f0ddc67ede5a6d171e710e0d1039fc
Evgeny Ratnikov <ratnikov.ev@gmail.com>
2009-06-15 10:04:12 +0400

    action in FilteringJTree

--- java/jtree/FilteringJTree.java
+++ java/jtree/FilteringJTree.java
@@ -1,3 +1,5 @@
+import javax.swing.KeyStroke;
+import javax.swing.JComponent;
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -15,17 +17,37 @@ import java.net.URL;
 import java.io.IOException;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
-public class FilteringJTree extends JPanel {
+public class FilteringJTree extends JPanel implements ActionListener {
     private JTree tree;
+    private JPanel filterPanel;
 
     public FilteringJTree(DefaultMutableTreeNode top) {
         super(new GridLayout(1,0));
         tree = new JTree(top);
-        add(tree);
+        tree.setPreferredSize(new Dimension(500, 200));
+
+        tree.registerKeyboardAction(this, KeyStroke.getKeyStroke('/'), JComponent.WHEN_FOCUSED);
+        tree.registerKeyboardAction(this, KeyStroke.getKeyStroke('a'), JComponent.WHEN_FOCUSED);
+        tree.registerKeyboardAction(this, KeyStroke.getKeyStroke('b'), JComponent.WHEN_FOCUSED);
+
+        filterPanel = new JPanel();
+        filterPanel.setVisible(false);
+        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, tree, filterPanel);
+        splitPane.setPreferredSize(new Dimension(500, 300));
+
+        add(splitPane);
     }
     public TreeSelectionModel getSelectionModel() {
         return tree.getSelectionModel();
+    }
+
+    // Event handler --------------------------------------------------------
+    public void actionPerformed (ActionEvent e)
+    {
+        filterPanel.setVisible(!filterPanel.isVisible());
     }
 }
