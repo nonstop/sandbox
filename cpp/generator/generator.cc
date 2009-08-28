5e82c15fb040083b783568de6ae0d7afb489658d
Evgeny Ratnikov <ratnikov.ev@gmail.com>
2009-08-28 14:48:31 +0400

    ReadFileByLine

--- cpp/generator/generator.cc
+++ cpp/generator/generator.cc
@@ -6,11 +6,11 @@
 
 #include "generator.h"
 
-struct TextFileReaderTraits
+struct ReadFileByLine
 {
     typedef const char* init_type;
     typedef std::string value_type;
-    TextFileReaderTraits(init_type filename) : file_(filename) {
+    ReadFileByLine(init_type filename) : file_(filename) {
         DEBUG(__FUNCTION__);
     }
     value_type yield() {
@@ -33,11 +33,11 @@ struct TextFileReaderTraits
 private:
     std::ifstream file_;
 };
-typedef generator<TextFileReaderTraits> text_file_reader;
 
 int main()
 {
-    text_file_reader lineReader("generator.cc");
+    typedef generator<ReadFileByLine> file_reader;
+    file_reader lineReader("generator.cc");
 
     size_t i = 0;
     BOOST_FOREACH(const std::string& line, lineReader) {
