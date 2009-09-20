f50380a17ec72fed6066311d1bce362b5086cfcd
Evgeny Ratnikov <ratnikov.ev@gmail.com>
2009-09-20 21:47:07 +0400

    dispatcher with clients: one-by-one

--- networking/complex/dispatcher.cc
+++ networking/complex/dispatcher.cc
@@ -1,7 +1,8 @@
-#include <iostream>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
+
+#include "utils.h"
 
 using boost::asio::ip::udp;
 
@@ -19,17 +20,19 @@ public:
 
     void handle_receive_from(const boost::system::error_code& error, size_t bytes_recvd) {
         if (!error && bytes_recvd > 0) {
-            cnt_ += bytes_recvd;
-            memset(reply_, 0, sizeof(reply_));
-            strcpy(reply_, "Reply on: ");
-            strncat(reply_, data_, 10);
+            if (strncmp(data_, "STOP", 4) == 0) {
+                TRACE("stopping Dispatcher");
+                return;
+            }
+            TRACE("req: %.10s", data_);
+            strcpy(reply_, "No answer");
             socket_.async_send_to(
                     boost::asio::buffer(reply_, strlen(reply_)), sender_endpoint_,
                     boost::bind(&Dispatcher::handle_send_to, this,
                         boost::asio::placeholders::error,
                         boost::asio::placeholders::bytes_transferred));
         } else {
-            std::cerr << "error: " << error << " bytes_recvd: " << bytes_recvd << std::endl;
+            TRACE_STREAM("error: " << error << " bytes_recvd: " << bytes_recvd);
             socket_.async_receive_from(
                     boost::asio::buffer(data_, max_length), sender_endpoint_,
                     boost::bind(&Dispatcher::handle_receive_from, this,
@@ -51,21 +54,19 @@ private:
     enum { max_length = 65000 };
     char data_[max_length];
     char reply_[max_length];
-    int cnt_;
 };
 
 int main(int argc, char* argv[])
 {
     try {
         if (argc != 3) {
-            std::cerr << "Usage: displatcher clientPort handlersPort\n";
+            TRACE_STREAM("Usage: displatcher clientPort handlersPort");
             return 1;
         }
         const short clientPort = atoi(argv[1]);
         const short handlersPort = atoi(argv[2]);
-        if (!clientPort || handlersPort) {
-            std::cerr << "Bad args: clientPort=" << argv[1]
-                << " handlersPort=" << argv[2] << std::endl;
+        if (!clientPort || !handlersPort) {
+            TRACE_STREAM("Bad args: clientPort=" << argv[1] << " handlersPort=" << argv[2]);
             return 1;
         }
         boost::asio::io_service io_service;
@@ -74,9 +75,10 @@ int main(int argc, char* argv[])
 
         io_service.run();
     } catch (const std::exception& e) {
-        std::cerr << "Exception: " << e.what() << "\n";
+        TRACE_STREAM("Exception: " << e.what());
     }
 
+    TRACE("Dispatcher stopped");
     return 0;
 }
 
