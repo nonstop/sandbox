eb1799e21dd0348fa286ba7559c6ecad656c0d01
Evgeny Ratnikov <ratnikov.ev@gmail.com>
2009-09-24 10:55:17 +0400

    date_time - format from argv

--- cpp/boost/date_time.cpp
+++ cpp/boost/date_time.cpp
@@ -9,22 +9,23 @@ using namespace std;
 using namespace boost::posix_time;
 
 
-string getDateTimeStr()
+string getDateTimeStr(const char* format)
 {
     boost::date_time::time_facet<ptime, char>* output_facet = new boost::date_time::time_facet<ptime, char>();
     stringstream ss;
-    ptime t(second_clock::local_time());
+    ptime t(microsec_clock::local_time());
 
     ss.imbue(locale(locale::classic(), output_facet));
-    output_facet->format("%y%m%d_%H_%M_%S");
+    output_facet->format(format);
     ss << t;
     return ss.str();
 }
 
-int main(int ac, char * av)
+int main(int ac, char * av[])
 {
     cout << "ctime is: " << time(0) << endl;
-    cout << "boost::date_time is: " << getDateTimeStr() << endl;
+    const char* format = (ac > 1 ? av[1] : "%y%m%d_%H_%M_%S%.2F");
+    cout << "format=[" << format << "] boost::date_time is: " << getDateTimeStr(format) << endl;
     return 0;
 }
 
