7c66d1a10b699eb53180d2023d01b66e51dc4c44
Evgeny Ratnikov <ratnikov.ev@gmail.com>
2009-12-25 22:53:12 +0300

    basic_unit

---
+++ opengl/giel/basic_unit.c
@@ -0,0 +1,207 @@
+#include <math.h>
+#include <GL/gl.h>
+
+#include "basic_unit.h"
+
+
+#define VOFFSET 0.045
+/* the triangular prism what makes up the basic unit */
+static const float solid_prism_v[][3] = {
+    /* first corner, bottom left front */
+    { VOFFSET, VOFFSET, 1.0 },
+    { VOFFSET, 0.00, 1.0 - VOFFSET },
+    { 0.00, VOFFSET, 1.0 - VOFFSET },
+    /* second corner, rear */
+    { VOFFSET, VOFFSET, 0.00 },
+    { VOFFSET, 0.00, VOFFSET },
+    { 0.00, VOFFSET, VOFFSET },
+    /* third, right front */
+    { 1.0 - VOFFSET / M_SQRT1_2, VOFFSET, 1.0 },
+    { 1.0 - VOFFSET / M_SQRT1_2, 0.0, 1.0 - VOFFSET },
+    { 1.0 - VOFFSET * M_SQRT1_2, VOFFSET, 1.0 - VOFFSET },
+    /* fourth, right rear */
+    { 1.0 - VOFFSET / M_SQRT1_2, VOFFSET, 0.0 },
+    { 1.0 - VOFFSET / M_SQRT1_2, 0.0, VOFFSET },
+    { 1.0 - VOFFSET * M_SQRT1_2, VOFFSET, VOFFSET },
+    /* fifth, upper front */
+    { VOFFSET, 1.0 - VOFFSET / M_SQRT1_2, 1.0 },
+    { VOFFSET / M_SQRT1_2, 1.0 - VOFFSET * M_SQRT1_2, 1.0 - VOFFSET },
+    { 0.0, 1.0 - VOFFSET / M_SQRT1_2, 1.0 - VOFFSET},
+    /* sixth, upper rear */
+    { VOFFSET, 1.0 - VOFFSET / M_SQRT1_2, 0.0 },
+    { VOFFSET / M_SQRT1_2, 1.0 - VOFFSET * M_SQRT1_2, VOFFSET },
+    { 0.0, 1.0 - VOFFSET / M_SQRT1_2, VOFFSET }};
+
+static const float solid_prism_n[][3] = {/* corners */
+    { -VOFFSET, -VOFFSET, VOFFSET },
+    { VOFFSET, -VOFFSET, VOFFSET },
+    { -VOFFSET, VOFFSET, VOFFSET },
+    { -VOFFSET, -VOFFSET, -VOFFSET },
+    { VOFFSET, -VOFFSET, -VOFFSET },
+    { -VOFFSET, VOFFSET, -VOFFSET },
+    /* edges */
+    { -VOFFSET, 0.0, VOFFSET },
+    { 0.0, -VOFFSET, VOFFSET },
+    { VOFFSET, VOFFSET, VOFFSET },
+    { -VOFFSET, 0.0, -VOFFSET },
+    { 0.0, -VOFFSET, -VOFFSET },
+    { VOFFSET, VOFFSET, -VOFFSET },
+    { -VOFFSET, -VOFFSET, 0.0 },
+    { VOFFSET, -VOFFSET, 0.0 },
+    { -VOFFSET, VOFFSET, 0.0 },
+    /* faces */
+    { 0.0, 0.0, 1.0 },
+    { 0.0, -1.0, 0.0 },
+    { M_SQRT1_2, M_SQRT1_2, 0.0 },
+    { -1.0, 0.0, 0.0 },
+    { 0.0, 0.0, -1.0 }};
+
+static const float wire_prism_v[][3] = {{ 0.0, 0.0, 1.0 },
+               { 1.0, 0.0, 1.0 },
+               { 0.0, 1.0, 1.0 },
+               { 0.0, 0.0, 0.0 },
+               { 1.0, 0.0, 0.0 },
+               { 0.0, 1.0, 0.0 }};
+
+void drawBasicUnit()
+{
+    // TODO
+    glBegin(GL_TRIANGLES);
+    glNormal3fv(solid_prism_n[0]);
+    glVertex3fv(solid_prism_v[0]);
+    glVertex3fv(solid_prism_v[2]);
+    glVertex3fv(solid_prism_v[1]);
+    
+    glNormal3fv(solid_prism_n[1]);
+    glVertex3fv(solid_prism_v[6]);
+    glVertex3fv(solid_prism_v[7]);
+    glVertex3fv(solid_prism_v[8]);
+    
+    glNormal3fv(solid_prism_n[2]);
+    glVertex3fv(solid_prism_v[12]);
+    glVertex3fv(solid_prism_v[13]);
+    glVertex3fv(solid_prism_v[14]);
+    
+    glNormal3fv(solid_prism_n[3]);
+    glVertex3fv(solid_prism_v[3]);
+    glVertex3fv(solid_prism_v[4]);
+    glVertex3fv(solid_prism_v[5]);
+    
+    glNormal3fv(solid_prism_n[4]);
+    glVertex3fv(solid_prism_v[9]);
+    glVertex3fv(solid_prism_v[11]);
+    glVertex3fv(solid_prism_v[10]);
+    
+    glNormal3fv(solid_prism_n[5]);
+    glVertex3fv(solid_prism_v[16]);
+    glVertex3fv(solid_prism_v[15]);
+    glVertex3fv(solid_prism_v[17]);
+    glEnd();
+    /* edges */
+    glBegin(GL_QUADS);
+    glNormal3fv(solid_prism_n[6]);
+    glVertex3fv(solid_prism_v[0]);
+    glVertex3fv(solid_prism_v[12]);
+    glVertex3fv(solid_prism_v[14]);
+    glVertex3fv(solid_prism_v[2]);
+    
+    glNormal3fv(solid_prism_n[7]);
+    glVertex3fv(solid_prism_v[0]);
+    glVertex3fv(solid_prism_v[1]);
+    glVertex3fv(solid_prism_v[7]);
+    glVertex3fv(solid_prism_v[6]);
+    
+    glNormal3fv(solid_prism_n[8]);
+    glVertex3fv(solid_prism_v[6]);
+    glVertex3fv(solid_prism_v[8]);
+    glVertex3fv(solid_prism_v[13]);
+    glVertex3fv(solid_prism_v[12]);
+    
+    glNormal3fv(solid_prism_n[9]);
+    glVertex3fv(solid_prism_v[3]);
+    glVertex3fv(solid_prism_v[5]);
+    glVertex3fv(solid_prism_v[17]);
+    glVertex3fv(solid_prism_v[15]);
+    
+    glNormal3fv(solid_prism_n[10]);
+    glVertex3fv(solid_prism_v[3]);
+    glVertex3fv(solid_prism_v[9]);
+    glVertex3fv(solid_prism_v[10]);
+    glVertex3fv(solid_prism_v[4]);
+    
+    glNormal3fv(solid_prism_n[11]);
+    glVertex3fv(solid_prism_v[15]);
+    glVertex3fv(solid_prism_v[16]);
+    glVertex3fv(solid_prism_v[11]);
+    glVertex3fv(solid_prism_v[9]);
+    
+    glNormal3fv(solid_prism_n[12]);
+    glVertex3fv(solid_prism_v[1]);
+    glVertex3fv(solid_prism_v[2]);
+    glVertex3fv(solid_prism_v[5]);
+    glVertex3fv(solid_prism_v[4]);
+    
+    glNormal3fv(solid_prism_n[13]);
+    glVertex3fv(solid_prism_v[8]);
+    glVertex3fv(solid_prism_v[7]);
+    glVertex3fv(solid_prism_v[10]);
+    glVertex3fv(solid_prism_v[11]);
+    
+    glNormal3fv(solid_prism_n[14]);
+    glVertex3fv(solid_prism_v[13]);
+    glVertex3fv(solid_prism_v[16]);
+    glVertex3fv(solid_prism_v[17]);
+    glVertex3fv(solid_prism_v[14]);
+    glEnd();
+    
+    /* faces */
+    glBegin(GL_TRIANGLES);
+    glNormal3fv(solid_prism_n[15]);
+    glVertex3fv(solid_prism_v[0]);
+    glVertex3fv(solid_prism_v[6]);
+    glVertex3fv(solid_prism_v[12]);
+    
+    glNormal3fv(solid_prism_n[19]);
+    glVertex3fv(solid_prism_v[3]);
+    glVertex3fv(solid_prism_v[15]);
+    glVertex3fv(solid_prism_v[9]);
+    glEnd();
+    
+    glBegin(GL_QUADS);
+    glNormal3fv(solid_prism_n[16]);
+    glVertex3fv(solid_prism_v[1]);
+    glVertex3fv(solid_prism_v[4]);
+    glVertex3fv(solid_prism_v[10]);
+    glVertex3fv(solid_prism_v[7]);
+    
+    glNormal3fv(solid_prism_n[17]);
+    glVertex3fv(solid_prism_v[8]);
+    glVertex3fv(solid_prism_v[11]);
+    glVertex3fv(solid_prism_v[16]);
+    glVertex3fv(solid_prism_v[13]);
+    
+    glNormal3fv(solid_prism_n[18]);
+    glVertex3fv(solid_prism_v[2]);
+    glVertex3fv(solid_prism_v[14]);
+    glVertex3fv(solid_prism_v[17]);
+    glVertex3fv(solid_prism_v[5]);
+    glEnd();
+    
+    glBegin(GL_LINE_STRIP);
+    glVertex3fv(wire_prism_v[0]);
+    glVertex3fv(wire_prism_v[1]);
+    glVertex3fv(wire_prism_v[2]);
+    glVertex3fv(wire_prism_v[0]);
+    glVertex3fv(wire_prism_v[3]);
+    glVertex3fv(wire_prism_v[4]);
+    glVertex3fv(wire_prism_v[5]);
+    glVertex3fv(wire_prism_v[3]);
+    glEnd();
+    glBegin(GL_LINES);
+    glVertex3fv(wire_prism_v[1]);
+    glVertex3fv(wire_prism_v[4]);
+    glVertex3fv(wire_prism_v[2]);
+    glVertex3fv(wire_prism_v[5]);
+    glEnd();
+}
+
---
+++ opengl/giel/basic_unit.h
@@ -0,0 +1,9 @@
+#ifndef GIEL_BASIC_UNIT_H
+#define GIEL_BASIC_UNIT_H
+
+enum BasicUnitRotation {BUR_NONE, BUR_RIGHT, BUR_LEFT};
+
+void drawBasicUnit();
+
+#endif /* GIEL_BASIC_UNIT_H */
+
