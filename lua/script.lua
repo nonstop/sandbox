a254ac62c022295d25f4d4ec0f9848ac8344de09
Evgeny Ratnikov <ratnikov.ev@gmail.com>
2010-11-20 22:01:45 +0300

    print_table recursive

--- lua/script.lua
+++ lua/script.lua
@@ -9,3 +9,8 @@ print(bar2)
 bar[5] = 1
 print(bar[5])
 
+table = {}
+table.first = "one"
+table.second = {uno = "-1-"; duo = "-2-"; tres = "-3"; quatro = {a = 1; b = 2; c = 3}}
+table['3'] = 123
+printTable(table)
