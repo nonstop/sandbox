f6d2a156a5b98a9882dcc4c3dbaf2eed86f9f72b
Evgeny Ratnikov <ratnikov.ev@gmail.com>
2011-06-15 23:55:36 +0400

    menu stuc

--- opengl/arcomage/main.c
+++ opengl/arcomage/main.c
@@ -4,12 +4,10 @@
 #include <GL/glu.h>
 #include <SDL.h>
 
+#include "menu.h"
+
 #define SCREEN_WIDTH  640
 #define SCREEN_HEIGHT 480
-
-/* Define our booleans */
-#define TRUE  1
-#define FALSE 0
 
 static void arcomage_quit(int returnCode)
 {
@@ -19,24 +17,17 @@ static void arcomage_quit(int returnCode)
 
 static int resizeWindow(int width, int height)
 {
-    /* Height / width ration */
-    GLfloat ratio;
-
-    /* Protect against a divide by zero */
     if (height == 0) {
         height = 1;
     }
 
-    ratio = (GLfloat)width / (GLfloat)height;
-
-    /* Setup our viewport. */
     glViewport(0, 0, (GLsizei)width, (GLsizei)height);
 
     /* change to the projection matrix and set our viewing volume. */
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
-    /* Set our perspective */
+    const GLfloat ratio = (GLfloat)width / (GLfloat)height;
     gluPerspective(45.0f, ratio, 0.1f, 100.0f);
 
     /* Make sure we're chaning the model view and not the projection */
@@ -66,7 +57,7 @@ static void initGL(GLvoid)
     /* Enable smooth shading */
     glShadeModel(GL_SMOOTH);
     /* Set the background black */
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(0.0f, 1.0f, 0.0f, 0.0f);
     /* Depth buffer setup */
     glClearDepth(1.0f);
     /* Enables Depth Testing */
@@ -75,59 +66,32 @@ static void initGL(GLvoid)
     glDepthFunc(GL_LEQUAL);
     /* Really Nice Perspective Calculations */
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+    menu_init();
 }
 
-/* Here goes our drawing code */
+
 static void drawGLScene()
 {
-    /* These are to calculate our fps */
-    static GLint T0     = 0;
-    static GLint Frames = 0;
-
-    /* Clear The Screen And The Depth Buffer */
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    /* Move Left 1.5 Units And Into The Screen 6.0 */
     glLoadIdentity();
-    glTranslatef(-1.5f, 0.0f, -6.0f);
-
-    glBegin(GL_TRIANGLES);               /* Drawing Using Triangles       */
-    glColor3f(1.0f,  0.0f,  0.0f);     /* Red                           */
-    glVertex3f(0.0f,  1.0f,  0.0f);    /* Top Of Triangle               */
-    glColor3f(0.0f,  1.0f,  0.0f);     /* Green                         */
-    glVertex3f(-1.0f, -1.0f,  0.0f);   /* Left Of Triangle              */
-    glColor3f(0.0f,  0.0f,  1.0f);     /* Blue                          */
-    glVertex3f(1.0f, -1.0f,  0.0f);    /* Right Of Triangle             */
-    glEnd();                             /* Finished Drawing The Triangle */
-
-    /* Move Right 3 Units */
-    glTranslatef(3.0f, 0.0f, 0.0f);
+    glTranslatef(0, 0, -36.3f);
+    /*glTranslatef(0, 0, -3.3f);*/
 
     /* Set The Color To Blue One Time Only */
     glColor3f(0.5f, 0.5f, 1.0f);
 
-    glBegin(GL_QUADS);                   /* Draw A Quad              */
-    glVertex3f(1.0f,  1.0f,  0.0f);    /* Top Right Of The Quad    */
-    glVertex3f(-1.0f,  1.0f,  0.0f);   /* Top Left Of The Quad     */
-    glVertex3f(-1.0f, -1.0f,  0.0f);   /* Bottom Left Of The Quad  */
-    glVertex3f(1.0f, -1.0f,  0.0f);    /* Bottom Right Of The Quad */
-    glEnd();                             /* Done Drawing The Quad    */
+    const GLfloat width = 20.0f, height = 15.0f;
+    glBegin(GL_QUADS);
+    glVertex3f(-width,  height,  0.0f);
+    glVertex3f(width,  height,  0.0f);
+    glVertex3f(width, -height,  0.0f);
+    glVertex3f(-width, -height,  0.0f);
+    glEnd();
 
-    /* Draw it to the screen */
+    menu_draw();
+
     SDL_GL_SwapBuffers();
-
-    /* Gather our frames per second */
-    Frames++;
-    {
-        GLint t = SDL_GetTicks();
-        if (t - T0 >= 5000) {
-            GLfloat seconds = (t - T0) / 1000.0;
-            GLfloat fps = Frames / seconds;
-            printf("%d frames in %g seconds = %g FPS\n", Frames, seconds, fps);
-            T0 = t;
-            Frames = 0;
-        }
-    }
 }
 
 int main(int ac, char* av[])
