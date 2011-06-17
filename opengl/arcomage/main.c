9c24691a56415cf2a3a3e64caaad448a790d6395
Evgeny Ratnikov <ratnikov.ev@gmail.com>
2011-06-17 08:34:39 +0400

    primitive towers and walls

--- opengl/arcomage/main.c
+++ opengl/arcomage/main.c
@@ -4,10 +4,13 @@
 #include <GL/glu.h>
 #include <SDL.h>
 
+#include "utils.h"
 #include "menu.h"
 
-#define SCREEN_WIDTH  640
-#define SCREEN_HEIGHT 480
+typedef struct Screen
+{
+    int width, height;
+} Screen;
 
 static void arcomage_quit(int returnCode)
 {
@@ -15,7 +18,7 @@ static void arcomage_quit(int returnCode)
     exit(returnCode);
 }
 
-static int resizeWindow(int width, int height)
+static void resizeWindow(int width, int height)
 {
     if (height == 0) {
         height = 1;
@@ -23,19 +26,11 @@ static int resizeWindow(int width, int height)
 
     glViewport(0, 0, (GLsizei)width, (GLsizei)height);
 
-    /* change to the projection matrix and set our viewing volume. */
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-
-    const GLfloat ratio = (GLfloat)width / (GLfloat)height;
-    gluPerspective(45.0f, ratio, 0.1f, 100.0f);
-
-    /* Make sure we're chaning the model view and not the projection */
+    glOrtho(0, width, 0, height, -1, 1);
     glMatrixMode(GL_MODELVIEW);
-
-    /* Reset The View */
     glLoadIdentity();
-    return 1;
 }
 
 static int handleKeyPress(SDL_Surface* surface, SDL_keysym* keysym)
@@ -57,7 +52,7 @@ static void initGL(GLvoid)
     /* Enable smooth shading */
     glShadeModel(GL_SMOOTH);
     /* Set the background black */
-    glClearColor(0.0f, 1.0f, 0.0f, 0.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     /* Depth buffer setup */
     glClearDepth(1.0f);
     /* Enables Depth Testing */
@@ -70,24 +65,87 @@ static void initGL(GLvoid)
     menu_init();
 }
 
-
-static void drawGLScene()
+static void drawMesh(int width, int height)
 {
+    glColor3f(0.0f, 1.0f, 0.0f);
+    int i = 0;
+    const int xStep = width / 10;
+    for (; i <= 10; ++i) {
+        glBegin(GL_LINE_STRIP);
+        glVertex3f(i * xStep, 0, 0.0f);
+        glVertex3f(i * xStep, height, 0.0f);
+        glEnd();
+    }
+    const int yStep = height / 10;
+    for (i = 0; i <= 10; ++i) {
+        glBegin(GL_LINE_STRIP);
+        glVertex3f(0, i * yStep, 0.0f);
+        glVertex3f(width, i * yStep, 0.0f);
+        glEnd();
+    }
+}
+
+static void drawTower(int width, int height, int size)
+{
+    glPushMatrix();
+    const int xStep = width / 10;
+    const int yStep = height / 10;
+    glColor3f(0.0f, 0.0f, 1.0f);
+    const GLfloat w = 1.5 * xStep, h = 4 * yStep;
+    if (size > 0) {
+        glTranslatef((float)xStep / 2., yStep, 0.0);
+    } else {
+        glTranslatef(width - w - (float)xStep / 2., yStep, 0.0);
+    }
+    glBegin(GL_QUADS);
+    glVertex3f(0, h, 0);
+    glVertex3f(w, h, 0);
+    glVertex3f(w, 0, 0);
+    glVertex3f(0, 0, 0);
+    glEnd();
+    glBegin(GL_TRIANGLES);
+    glVertex3f(0, h, 0);
+    glVertex3f(w / 2., h + yStep, 0);
+    glVertex3f(w, h, 0);
+    glEnd();
+    glPopMatrix();
+}
+
+static void drawWall(int width, int height, int size)
+{
+    glPushMatrix();
+    const int xStep = width / 10;
+    const int yStep = height / 10;
+    glColor3f(0.0f, 0.0f, 0.5f);
+    const GLfloat w = 0.5 * xStep, h = 3 * yStep;
+    float delta = (float)(3 * xStep);
+    if (size > 0) {
+        glTranslatef(delta - w, yStep, 0.0);
+    } else {
+        glTranslatef(width - delta, yStep, 0.0);
+    }
+    glBegin(GL_QUADS);
+    glVertex3f(0, h, 0);
+    glVertex3f(w, h, 0);
+    glVertex3f(w, 0, 0);
+    glVertex3f(0, 0, 0);
+    glEnd();
+    glPopMatrix();
+}
+
+static void drawGLScene(const Screen* screen)
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-    glTranslatef(0, 0, -36.3f);
-    /*glTranslatef(0, 0, -3.3f);*/
+    /*glTranslatef(0, 0, -36.3f);*/
 
-    /* Set The Color To Blue One Time Only */
-    glColor3f(0.5f, 0.5f, 1.0f);
-
-    const GLfloat width = 20.0f, height = 15.0f;
-    glBegin(GL_QUADS);
-    glVertex3f(-width,  height,  0.0f);
-    glVertex3f(width,  height,  0.0f);
-    glVertex3f(width, -height,  0.0f);
-    glVertex3f(-width, -height,  0.0f);
-    glEnd();
+    drawMesh(screen->width, screen->height);
+    drawTower(screen->width, screen->height, 1);
+    drawTower(screen->width, screen->height, -1);
+    drawWall(screen->width, screen->height, 1);
+    drawWall(screen->width, screen->height, -1);
+    /*drawTower(screen->width, screen->height, -1);*/
 
     menu_draw();
 
@@ -123,14 +181,15 @@ int main(int ac, char* av[])
 
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
-    SDL_Surface* surface = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 16, videoFlags);
+    struct Screen screen = {640, 480};
+    SDL_Surface* surface = SDL_SetVideoMode(640, 480, 16, videoFlags);
     if (!surface) {
         fprintf(stderr,  "Video mode set failed: %s\n", SDL_GetError());
         arcomage_quit(1);
     }
 
     initGL();
-    resizeWindow(SCREEN_WIDTH, SCREEN_HEIGHT);
+    resizeWindow(screen.width, screen.height);
 
     int isActive = 1;
     int done = 0;
@@ -151,7 +210,7 @@ int main(int ac, char* av[])
                     fprintf(stderr, "Could not get a surface after resize: %s\n", SDL_GetError());
                     arcomage_quit(1);
                 }
-                resizeWindow(event.resize.w, event.resize.h);
+                resizeWindow(screen.width, screen.height);
                 break;
             case SDL_KEYDOWN:
                 done = handleKeyPress(surface, &event.key.keysym);
@@ -165,7 +224,7 @@ int main(int ac, char* av[])
         }
 
         if (isActive) {
-            drawGLScene();
+            drawGLScene(&screen);
         }
     }
 
